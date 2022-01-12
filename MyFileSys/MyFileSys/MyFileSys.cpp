de60df19ec814e7a00ee1ead5964ccaeea8bc858
Alisher Myrgyyassov <79082361+AlisherMyrgyyassov@users.noreply.github.com>
2022-01-13 00:02:07 +0800

    Add files via upload

---
+++ MyFileSys/MyFileSys/MyFileSys.cpp
@@ -0,0 +1,15 @@
+﻿#define _CRT_SECURE_NO_WARNINGS //disabling warnings caused by using "unsafe" functions
+
+#include "myFileSys.h"
+#include <iostream>
+
+
+int main()
+{
+    myFileSys* ff = new myFileSys;
+    ff->update(); //Obtaining the data from the existing txt files
+    ff->main_menu();
+    std::cout << "Thank you for using our program. Bye-bye!" << std::endl;
+    delete ff;
+    return 0;
+}
